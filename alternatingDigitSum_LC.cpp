f85d3d1807e83018d995d2a1ff70228ead48f460
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2023-03-29 15:13:50 +0530

    Alternating Digit Sum

---
+++ alternatingDigitSum_LC.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        vector<int>ans;
+        while(n!=0)
+        {
+            ans.push_back(n%10);
+            n=n/10;
+        }
+        reverse(ans.begin(),ans.end());
+        int sum=0;
+        bool kachhi=false;
+        for(int i=0;i<ans.size();i++)
+        {
+            if(kachhi==false){
+                sum=sum+ans[i];
+                kachhi=!kachhi;
+            }
+            else {
+                sum=sum+(-1)*ans[i];
+                kachhi=!kachhi;
+                cout<<sum<<endl;
+            }
+        }
+        return sum;
+    }
+};
