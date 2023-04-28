4bc64d52020b2ba08f86c2878cd1e651b4c21814
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2023-04-28 10:54:21 +0530

    28. Find the Index of the First Occurrence in a String  Leetcode

---
+++ lengthOfLastWord_LC.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && count)
+            {
+                break;
+            }
+            if (s[i] != ' ')
+                count++;
+        }
+
+        return count;
+    }
+};
